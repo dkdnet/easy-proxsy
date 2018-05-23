e626220a0b357f0352aa99211a4f813650a5b5a9
张冬冬 <13381120685@189.cn>
2018-05-23 20:14:09 +0800

    加了url 的rewrite功能

--- server.js
+++ server.js
@@ -9,21 +9,24 @@ app.use(cors());
 const router = express.Router();
 
 
-
 // 处理代理
-if(CONST.proxy) {
+if (CONST.proxy) {
     const proxyConfig = require('./config/server.proxy');
     proxyConfig.forEach(function (element) {
         if (element.enable) {//代理开启
             //默认配置项
             let proxyOpt = {
-            target: element.url,
-            logLevel: "debug",
-            changeOrigin: true,
-            headers: (typeof element.headers !== 'undefined' ? element.headers : {}),
-            onProxyRes: function (proxyRes) {
-                proxyRes.headers["Proxy-Server"] = "true";
-            }
+                target: element.url,
+                logLevel: "debug",
+                changeOrigin: true,
+                ws: true,                         // proxy websockets
+                pathRewrite: {
+                    '^/api/' : '/'           // remove base path
+                },
+                headers: (typeof element.headers !== 'undefined' ? element.headers : {}),
+                onProxyRes: function (proxyRes) {
+                    proxyRes.headers["Proxy-Server"] = "true";
+                }
             }
             app.use(element.router, proxy(proxyOpt));
             console.log(chalk.green(`[proxy] : ${element.router} to ${element.url}`));
@@ -40,7 +43,7 @@ if(CONST.proxy) {
 //     res.header("Content-Type", "application/json;charset=utf-8");
 //     next();
 // });
-router.all('*', function(req, res, next) {
+router.all('*', function (req, res, next) {
     // 设置跨域访问
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -52,10 +55,10 @@ router.all('*', function(req, res, next) {
 app.use(router);
 
 // 端口
-app.listen(CONST.port, function(err) {
+app.listen(CONST.port, function (err) {
     if (err) {
         console.log(err);
         return;
     }
-    console.log('Listening at http://localhost:'+ CONST.port);
+    console.log('Listening at http://localhost:' + CONST.port);
 });
